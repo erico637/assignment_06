4a7619c078cfb77d993fd0d7b587fbba71a80926
erico637 <efriesen@asu.edu>
2016-03-15 18:35:01 -0700

    Update point.py

---
+++ point.py
@@ -0,0 +1,23 @@
+
+Eric Friesenhahn
+
+
+class Point(object):
+ 
+     def __init__(self, x, y, mark):
+         self.x = x
+         self.y = y
+         self.mark = mark
+     def check_coincident(self, point):
+         return (self.x == point[0] and self.y == point[1])
+     def shift_point(self, x_shift, y_shift):
+         self.x += x_shift
+         self.y += y_shift
+     def getx(self):
+         return self.x
+     def gety(self):
+         return self.y
+     def getPoint(self):
+         return (self.x, self.y)
+     def getMark(self):
+         return self.mark
